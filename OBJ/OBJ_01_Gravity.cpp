e990902aebdd9253249ffea22729403e644b8f56
Xayah-Hina <ycxiang14sep@gmail.com>
2023-09-18 18:26:34 +0800

    update

--- OBJ/OBJ_01_Gravity.cpp
+++ OBJ/OBJ_01_Gravity.cpp
@@ -1,1 +1,27 @@
 #include "OBJ_01_Gravity.h"
+
+#include <PRM/PRM_Include.h>
+#include <OP/OP_Director.h>
+
+auto OBJ_01_Gravity::myConstructor(OP_Network *network, const char *name, OP_Operator *op) -> OP_Node * { return new OBJ_01_Gravity(network, name, op); }
+auto OBJ_01_Gravity::buildTemplatePair(OP_TemplatePair *prevstuff) -> OP_TemplatePair *
+{
+	return nullptr;
+}
+OBJ_01_Gravity::OBJ_01_Gravity(OP_Network *network, const char *name, OP_Operator *op) : OBJ_Geometry(network, name, op) {}
+
+void newObjectOperator(OP_OperatorTable *table)
+{
+	table->addOperator(
+			new OP_Operator(
+					"hdk_gravity",
+					"Gravity",
+					OBJ_01_Gravity::myConstructor,
+					OBJ_01_Gravity::buildTemplatePair(nullptr),
+					1,
+					1,
+					nullptr,
+					OP_FLAG_GENERATOR
+			)
+		);
+}
--- OBJ/OBJ_01_Gravity.h
+++ OBJ/OBJ_01_Gravity.h
@@ -1,9 +1,17 @@
 #ifndef LEARNHDK_OBJ_01_GRAVITY_H
 #define LEARNHDK_OBJ_01_GRAVITY_H
 
-class OBJ_01_Gravity
-{
+#include <OBJ/OBJ_Geometry.h>
 
+class OBJ_01_Gravity : public OBJ_Geometry
+{
+public:
+	// static constructor
+	static auto myConstructor(OP_Network *network, const char *name, OP_Operator *op) -> OP_Node*;
+	// static parameters
+	static auto buildTemplatePair(OP_TemplatePair *prevstuff) -> OP_TemplatePair*;
+	// constructor
+	OBJ_01_Gravity(OP_Network *network, const char *name, OP_Operator *op);
 };
 
 #endif //LEARNHDK_OBJ_01_GRAVITY_H
