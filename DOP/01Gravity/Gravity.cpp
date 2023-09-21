ec6ecabee7470aaeca45b84539409b16f54c85a9
Xayah-Hina <ycxiang14sep@gmail.com>
2023-09-21 17:21:54 +0800

    update

--- DOP/01Gravity/Gravity.cpp
+++ DOP/01Gravity/Gravity.cpp
@@ -1,1 +1,32 @@
 #include "Gravity.h"
+#include <PRM/PRM_Include.h>
+#include <SIM/SIM_DopDescription.h>
+
+auto GravitySolver::solveSingleObjectSubclass(SIM_Engine &engine, SIM_Object &object, SIM_ObjectArray &feedbacktoobjects, const SIM_Time &timestep, bool newobject) -> SIM_Solver::SIM_Result
+{
+	return SIM_SOLVER_SUCCESS;
+}
+
+auto GravitySolver::getMyOwnSolverDescription() -> const SIM_DopDescription *
+{
+	static PRM_Name	 param1("param1", "Param 1");
+	static PRM_Name	 param2("param2", "Param 2");
+	static PRM_Name	 param3("param3", "Param 3");
+
+	static PRM_Template	 theTemplates[] = {
+			PRM_Template(PRM_INT,		3, &param1, PRMtenDefaults),
+			PRM_Template(PRM_XYZ,		3, &param2, PRMzeroDefaults),
+			PRM_Template(PRM_XYZ,		3, &param3, PRMoneDefaults),
+			PRM_Template()
+	};
+
+	static SIM_DopDescription	 theDopDescription(true,
+													"hdk_mygravitysolver",
+													"My Gravity Solver",
+													"MyGravitySolver",
+													classname(),
+													theTemplates);
+
+	return &theDopDescription;
+}
+GravitySolver::GravitySolver(const SIM_DataFactory *factory) : SIM_SingleSolver(factory), SIM_OptionsUser(this) {}
--- DOP/01Gravity/Gravity.h
+++ DOP/01Gravity/Gravity.h
@@ -1,9 +1,26 @@
 #ifndef LEARNHDKDEV_GRAVITY_H
 #define LEARNHDKDEV_GRAVITY_H
 
-class Gravity
-{
+#include <SIM/SIM_SingleSolver.h>
+#include <SIM/SIM_OptionsUser.h>
+#include <UT/UT_DSOVersion.h> // must include
 
+class GravitySolver : public SIM_SingleSolver, public SIM_OptionsUser
+{
+public:
+	explicit GravitySolver(const SIM_DataFactory *factory);
+
+protected:
+	auto solveSingleObjectSubclass(SIM_Engine &engine, SIM_Object &object, SIM_ObjectArray &feedbacktoobjects, const SIM_Time &timestep, bool newobject) -> SIM_Result override;
+
+private:
+	static auto getMyOwnSolverDescription() -> const SIM_DopDescription *;
+
+	DECLARE_STANDARD_GETCASTTOTYPE();
+	DECLARE_DATAFACTORY(GravitySolver,
+						SIM_SingleSolver,
+						"Gravity Solver",
+						getMyOwnSolverDescription());
 };
 
 #endif //LEARNHDKDEV_GRAVITY_H
